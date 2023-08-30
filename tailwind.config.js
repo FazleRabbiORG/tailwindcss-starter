5758113aac8602fc50e6b33c8861008518381570
fazlerabbi092 <24.fazlerabbi@gmail.com>
2023-08-31 01:04:28 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    fontFamily: {
+      body: ["Roboto", "sans-serif"],
+    },
+  },
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
